df162f1e555527f5e5c5f613f5153c9b2ece7c05
gignu <linus.komnick@googlemail.com>
2020-10-31 13:11:09 +0100

    Some fixes for deployment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import UserState from './context/UserState'
 
-ReactDOM.render(
-  <UserState>
-    <App />
-  </UserState>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
